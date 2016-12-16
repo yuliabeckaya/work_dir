f875460c3ebd3a45135e22dcc08d7dc41fd1fc77
yuliabeckaya <yuliabeckaya@gmail.com>
2016-12-16 09:31:58 +0300

    1 verse

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,9 @@
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+
---
+++ source/js/preloader.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+});
+
+$(function (){
+
+});
+
+$(window).on('load', function (){
+
+});
+
+$(window).on('resize', function () {
+	//$(window).width();
+}); 
+
+$(window).on ('scroll', function () {
+
+	});
+
